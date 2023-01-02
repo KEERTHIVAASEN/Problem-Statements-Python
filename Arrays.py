d612812d4a2822b346512df558b97cc4957bafec
KEERTHI  VAASEN <84181432+KEERTHIVAASEN@users.noreply.github.com>
2023-01-02 13:11:22 +0000

    up

---
+++ Arrays.py
@@ -0,0 +1,26 @@
+# Declare an array with some initial values
+my_array = [1, 2, 3, 4, 5]
+
+# Print the entire array
+print(my_array)
+
+# Access and print an element of the array by its index
+print(my_array[2])
+
+# Modify an element of the array by its index
+my_array[2] = 6
+
+# Append an element to the end of the array
+my_array.append(7)
+
+# Insert an element at a specific position in the array
+my_array.insert(3, 8)
+
+# Remove and return an element from the end of the array
+last_element = my_array.pop()
+
+# Remove an element from the array by its value
+my_array.remove(8)
+
+# Print the modified array
+print(my_array)

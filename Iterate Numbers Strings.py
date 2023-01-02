d06a6b4b5f9dfb2da2752459269feb9396692f31
KEERTHI  VAASEN <84181432+KEERTHIVAASEN@users.noreply.github.com>
2023-01-02 13:40:00 +0000

    up

---
+++ Iterate Numbers Strings.py
@@ -0,0 +1,22 @@
+# Use a for loop to iterate over a range of numbers
+for i in range(1,11):
+    print(i)
+
+# Output:
+# 0
+# 1 ...
+
+# Path: Iterate Numbers Strings.py
+# Use a for loop to iterate over a range of numbers with a step 
+
+# Declare a list of strings
+words = ["Cat", "Dog", "Fish"]
+
+# Use a for loop to iterate over the list
+for word in words:
+    print(word)
+
+# Output:
+# cat
+# dog
+# fish

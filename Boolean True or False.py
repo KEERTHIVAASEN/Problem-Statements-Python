d4a8a2c630c86aa61e9f8effebcc3b3a356cbfdb
KEERTHI  VAASEN <84181432+KEERTHIVAASEN@users.noreply.github.com>
2023-01-02 13:00:16 +0000

    update

---
+++ Boolean True or False.py
@@ -0,0 +1,19 @@
+# Declare two variables and assign them Boolean values
+var1 = True
+var2 = False
+
+# Print the value of each variable
+print("var1:", var1)
+print("var2:", var2)
+
+# Use the "and" operator to test both variables
+result = var1 and var2
+print("var1 and var2:", result)
+
+# Use the "or" operator to test both variables
+result = var1 or var2
+print("var1 or var2:", result)
+
+# Use the "not" operator to negate a Boolean value
+result = not var1
+print("not var1:", result)

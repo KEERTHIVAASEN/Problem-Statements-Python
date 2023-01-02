68af37de2d428363d701ef3d64dd156134d7d25c
KEERTHI  VAASEN <84181432+KEERTHIVAASEN@users.noreply.github.com>
2023-01-02 13:05:59 +0000

    up

---
+++ Numbers Strings.py
@@ -0,0 +1,47 @@
+# Declare and assign values to two integer variables
+int1 = 10
+int2 = 20
+
+# Declare and assign values to two floating-point variables
+float1 = 1.5
+float2 = 2.5
+
+# Declare and assign values to two string variables
+string1 = "Hello"
+string2 = "World"
+
+# Add the two integers and assign the result to a new variable
+result = int1 + int2
+print("int1 + int2:", result)
+
+# Subtract the two integers and assign the result to a new variable
+result = int1 - int2
+print("int1 - int2:", result)
+
+# Multiply the two integers and assign the result to a new variable
+result = int1 * int2
+print("int1 * int2:", result)
+
+# Divide the two integers and assign the result to a new variable
+result = int1 / int2
+print("int1 / int2:", result)
+
+# Add the two floating-point numbers and assign the result to a new variable
+result = float1 + float2
+print("float1 + float2:", result)
+
+# Subtract the two floating-point numbers and assign the result to a new variable
+result = float1 - float2
+print("float1 - float2:", result)
+
+# Multiply the two floating-point numbers and assign the result to a new variable
+result = float1 * float2
+print("float1 * float2:", result)
+
+# Divide the two floating-point numbers and assign the result to a new variable
+result = float1 / float2
+print("float1 / float2:", result)
+
+# Concatenate the two strings and assign the result to a new variable
+result = string1 + string2
+print("string1 + string2:", result)
